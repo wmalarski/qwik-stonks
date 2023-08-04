6f40f83512fde076fc4ebccb6163d01bbe9f652d
Wojciech Malarski <w.malarski@gmail.com>
2023-08-04 22:35:10 +0200

    cleaning up demo staff

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,12 +1,21 @@
 import { component$ } from "@builder.io/qwik";
 import { useServerTimeLoader } from "~/routes/layout";
+import { css } from "~/styled-system/css";
 import styles from "./footer.module.css";
 
 export default component$(() => {
   const serverTime = useServerTimeLoader();
 
   return (
-    <footer>
+    <footer
+      class={css({
+        padding: 10,
+        bg: "red.400",
+        height: "dvh",
+        margin: 100,
+        fontSize: 30,
+      })}
+    >
       <div class="container">
         <a href="https://www.builder.io/" target="_blank" class={styles.anchor}>
           <span>Made with ♡ by Builder.io</span>
