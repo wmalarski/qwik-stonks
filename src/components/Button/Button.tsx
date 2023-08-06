afb3957ac6b90fd1a981f3ac37367ad165fd5e27
Wojciech Malarski <w.malarski@gmail.com>
2023-08-06 13:14:33 +0200

    button finished for now

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,18 +4,34 @@ import { styled } from "~/styled-system/jsx";
 const buttonVariants = cva({
   base: {
     display: "inline-flex",
+    flexShrink: 0,
+    cursor: "pointer",
+    userSelect: "none",
+    flexWrap: "wrap",
     alignItems: "center",
     justifyContent: "center",
-    borderRadius: "md",
-    fontSize: "sm",
-    fontWeight: "medium",
-    // ring-offset-background transition-colors
+    borderColor: ["transparent", "base.200"],
+    textAlign: "center",
+    borderRadius: "sm",
+    height: "3rem",
+    paddingLeft: "1rem",
+    paddingRight: "1rem",
+    fontSize: ".875rem",
+    lineHeight: ["1.25rem", "1em"],
+    minHeight: "3rem",
+    gap: ".5rem",
+    fontWeight: 600,
+    textDecorationLine: "none",
+    borderWidth: "thin",
+
+    outlineColor: "base.content",
+
     _focusVisible: {
-      outline: "none",
-      ringOffset: "0.5",
-      ringColor: "red.200",
-      ringWidth: "0.5",
+      outlineStyle: "solid",
+      outlineWidth: "2px",
+      outlineOffset: "2px",
     },
+
     _disabled: {
       opacity: 0.5,
       pointerEvents: "none",
@@ -24,43 +40,59 @@ const buttonVariants = cva({
   variants: {
     variant: {
       default: {
-        backgroundColor: "bg-primary",
-        color: "text-primary-foreground",
+        backgroundColor: "base.200",
+        color: "base.content",
         _hover: {
-          backgroundColor: "bg-primary/90",
+          borderColor: "base.300",
+          backgroundColor: "base.300",
         },
       },
-      outline: {
-        border:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+      neutral: {
+        borderColor: "neutral",
+        backgroundColor: "neutral",
+        color: "neutral.content",
+        outlineColor: "neutral",
+        _hover: {
+          borderColor: "neutral.focus",
+          backgroundColor: "neutral.focus",
+        },
+      },
+      primary: {
+        borderColor: "primary",
+        backgroundColor: "primary",
+        color: "primary.content",
+        outlineColor: "primary",
+        _hover: {
+          borderColor: "primary.focus",
+          backgroundColor: "primary.focus",
+        },
       },
       secondary: {
-        backgroundColor: "bg-secondary",
-        color: "text-secondary-foreground",
+        borderColor: "secondary",
+        backgroundColor: "secondary",
+        color: "secondary.content",
+        outlineColor: "secondary",
         _hover: {
-          backgroundColor: "bg-secondary/80",
+          borderColor: "secondary.focus",
+          backgroundColor: "secondary.focus",
         },
       },
-      ghost: {
+      accent: {
+        borderColor: "accent",
+        backgroundColor: "accent",
+        color: "accent.content",
+        outlineColor: "accent",
         _hover: {
-          backgroundColor: "hover:bg-accent",
-          color: "text-accent-foreground",
-        },
-      },
-      link: {
-        //  underline-offset-4 hover:underline
-        color: "text-primary",
-        textUnderlineOffset: 4,
-        _hover: {
-          textUnderlinePosition: "under",
+          borderColor: "accent.focus",
+          backgroundColor: "accent.focus",
         },
       },
     },
     size: {
-      default: { height: "h-10 px-4 py-2" },
-      sm: { height: "h-9 rounded-md px-3" },
-      lg: { height: "h-11 rounded-md px-8" },
-      icon: { height: "h-10 w-10" },
+      default: { height: "10", px: "4", py: "2" },
+      sm: { height: "9", borderRadius: "md", px: "3" },
+      lg: { height: "11", borderRadius: "md", px: "8" },
+      icon: { height: "10", width: "10" },
     },
   },
   defaultVariants: {
