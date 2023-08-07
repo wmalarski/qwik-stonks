716e5d84dbb85bee44f6407448699cdb3be39743
Wojciech Malarski <w.malarski@gmail.com>
2023-08-07 20:10:26 +0200

    cookies part added

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,25 +1,18 @@
+import type { RequestEventCommon } from "@builder.io/qwik-city";
 import { z } from "@builder.io/qwik-city";
-import type { RequestEvent } from "@builder.io/qwik-city/middleware/request-handler";
+import jwt from "jsonwebtoken";
 import { buildSearchParams } from "~/utils/searchParams";
-
-export const getAuthConfig = (env: RequestEvent["env"]) => {
-  return {
-    domain: env.get("PUBLIC_AUTH0_DOMAIN")!,
-    clientID: env.get("PUBLIC_AUTH0_CLIENT_ID")!,
-    redirectUri: env.get("PUBLIC_AUTH0_REDIRECT_URL")!,
-    clientSecret: env.get("AUTH0_CLIENT_SECRET")!,
-  };
-};
+import { getServerEnv } from "./env";
 
 type AuthFetchArgs = {
-  event: RequestEvent;
+  event: RequestEventCommon;
   init?: RequestInit;
   path: string;
   query?: Record<string, unknown>;
 };
 
 export const authFetch = (args: AuthFetchArgs) => {
-  const config = getAuthConfig(args.event.env);
+  const config = getServerEnv(args.event);
 
   const search = buildSearchParams(args.query);
 
@@ -33,13 +26,11 @@ export const getAuthTokenSchema = () => {
 };
 
 type GetAuthTokenArgs = z.infer<ReturnType<typeof getAuthTokenSchema>> & {
-  event: RequestEvent;
+  event: RequestEventCommon;
 };
 
 export const getAuthToken = async (args: GetAuthTokenArgs) => {
-  const config = getAuthConfig(args.event.env);
-
-  console.log("config", config);
+  const config = getServerEnv(args.event);
 
   const result = await authFetch({
     event: args.event,
@@ -62,4 +53,77 @@ export const getAuthToken = async (args: GetAuthTokenArgs) => {
   console.log("json", json);
 
   return json;
+};
+
+export type Session = {
+  userId: string;
+  iat: number;
+  exp: number;
+};
+
+const options = {
+  httpOnly: true,
+  maxAge: 0,
+  name: "__session",
+  path: "/",
+  sameSite: "lax",
+} as const;
+
+const SESSION_COOKIE_KEY = "__session";
+
+export const createSession = (event: RequestEventCommon, userId: string) => {
+  const env = getServerEnv(event);
+
+  const token = jwt.sign({ userId }, env.sessionSecret, { expiresIn: "7d" });
+
+  event.cookie.set(SESSION_COOKIE_KEY, token, {
+    ...options,
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+    secure: env.nodeEnv === "production",
+  });
+};
+
+export const deleteSession = (event: RequestEventCommon) => {
+  event.cookie.delete(SESSION_COOKIE_KEY, options);
+};
+
+const getSession = (event: RequestEventCommon): Session | null => {
+  const token = event.cookie.get(SESSION_COOKIE_KEY)?.value;
+  const env = getServerEnv(event);
+
+  if (!token) {
+    return null;
+  }
+
+  try {
+    const session = jwt.verify(token, env.sessionSecret);
+
+    return session as Session;
+  } catch (err) {
+    deleteSession(event);
+
+    return null;
+  }
+};
+
+const SESSION_CACHE_KEY = "__session";
+
+export const getRequestSession = (
+  event: RequestEventCommon,
+): Session | null => {
+  const value = event.sharedMap.get(SESSION_CACHE_KEY);
+
+  if (value) {
+    return value.session;
+  }
+
+  const session = getSession(event);
+
+  event.sharedMap.set(SESSION_CACHE_KEY, { session });
+
+  return session;
+};
+
+export const clearRequestSession = (event: RequestEventCommon) => {
+  event.sharedMap.set(SESSION_CACHE_KEY, { session: null });
 };
---
+++ src/server/env.ts
@@ -0,0 +1,39 @@
+import { z, type RequestEventCommon } from "@builder.io/qwik-city";
+
+const ENV_CACHE_KEY = "__env";
+
+const getServerEnvSchema = () => {
+  return z.object({
+    clientID: z.string(),
+    clientSecret: z.string(),
+    nodeEnv: z.string(),
+    domain: z.string(),
+    notionKey: z.string(),
+    redirectUri: z.string(),
+    sessionSecret: z.string(),
+  });
+};
+
+export const getServerEnv = (
+  event: RequestEventCommon,
+): z.infer<ReturnType<typeof getServerEnvSchema>> => {
+  const cached = event.sharedMap.get(ENV_CACHE_KEY);
+
+  if (cached) {
+    return cached;
+  }
+
+  const parsed = getServerEnvSchema().parse({
+    clientID: event.env.get("PUBLIC_AUTH0_CLIENT_ID"),
+    clientSecret: event.env.get("AUTH0_CLIENT_SECRET"),
+    domain: event.env.get("PUBLIC_AUTH0_DOMAIN"),
+    notionKey: event.env.get("NOTION_KEY"),
+    redirectUri: event.env.get("PUBLIC_AUTH0_REDIRECT_URL"),
+    sessionSecret: event.env.get("SESSION_SECRET"),
+    nodeEnv: event.env.get("NODE_ENV"),
+  });
+
+  event.sharedMap.set(ENV_CACHE_KEY, parsed);
+
+  return parsed;
+};
