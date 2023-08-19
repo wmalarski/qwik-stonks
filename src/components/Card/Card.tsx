ae5b607b24be7c18095a36f5f2d5126d47025b89
Wojciech Malarski <w.malarski@gmail.com>
2023-08-19 18:32:57 +0200

    card component updated

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,203 +3,33 @@ import { styled } from "~/styled-system/jsx";
 
 const cardVariants = cva({
   base: {
-    display: "inline-flex",
-    flexShrink: 0,
-    cursor: "pointer",
-    userSelect: "none",
-    flexWrap: "wrap",
-    alignItems: "center",
-    justifyContent: "center",
-    borderColor: ["transparent", "base.200"],
-    textAlign: "center",
-    borderRadius: "sm",
-    height: "3rem",
-    paddingLeft: "1rem",
-    paddingRight: "1rem",
-    fontSize: ".875rem",
-    lineHeight: ["1.25rem", "1em"],
-    minHeight: "3rem",
-    gap: ".5rem",
-    fontWeight: 600,
-    textDecorationLine: "none",
-    borderWidth: "thin",
+    position: "relative",
+    display: "flex",
+    flexDirection: "column",
+    borderRadius: "md",
+    boxShadow: "xl",
 
-    outlineColor: "base.content",
-
-    _focusVisible: {
-      outlineStyle: "solid",
-      outlineWidth: "2px",
+    _focus: {
+      outline: "2px solid transparent",
       outlineOffset: "2px",
     },
 
-    _disabled: {
-      opacity: 0.5,
-      pointerEvents: "none",
+    _focusVisible: {
+      outline: "2px solid currentColor",
+      outlineOffset: "2px",
     },
-  },
-  variants: {
-    variant: {
-      default: {
-        backgroundColor: "base.200",
-        color: "base.content",
-        _hover: {
-          borderColor: "base.300",
-          backgroundColor: "base.300",
-        },
-      },
-      neutral: {
-        borderColor: "neutral",
-        backgroundColor: "neutral",
-        color: "neutral.content",
-        outlineColor: "neutral",
-        _hover: {
-          borderColor: "neutral.focus",
-          backgroundColor: "neutral.focus",
-        },
-      },
-      primary: {
-        borderColor: "primary",
-        backgroundColor: "primary",
-        color: "primary.content",
-        outlineColor: "primary",
-        _hover: {
-          borderColor: "primary.focus",
-          backgroundColor: "primary.focus",
-        },
-      },
-      secondary: {
-        borderColor: "secondary",
-        backgroundColor: "secondary",
-        color: "secondary.content",
-        outlineColor: "secondary",
-        _hover: {
-          borderColor: "secondary.focus",
-          backgroundColor: "secondary.focus",
-        },
-      },
-      accent: {
-        borderColor: "accent",
-        backgroundColor: "accent",
-        color: "accent.content",
-        outlineColor: "accent",
-        _hover: {
-          borderColor: "accent.focus",
-          backgroundColor: "accent.focus",
-        },
-      },
-    },
-    size: {
-      default: { height: "10", px: "4", py: "2" },
-      sm: { height: "9", borderRadius: "md", px: "3" },
-      lg: { height: "11", borderRadius: "md", px: "8" },
-      icon: { height: "10", width: "10" },
-    },
-  },
-  defaultVariants: {
-    variant: "default",
-    size: "default",
   },
 });
 
 export const Card = styled("div", cardVariants);
 
-const cardBodyVariants = cva({
+export const cardBodyVariants = cva({
   base: {
-    display: "inline-flex",
-    flexShrink: 0,
-    cursor: "pointer",
-    userSelect: "none",
-    flexWrap: "wrap",
-    alignItems: "center",
-    justifyContent: "center",
-    borderColor: ["transparent", "base.200"],
-    textAlign: "center",
-    borderRadius: "sm",
-    height: "3rem",
-    paddingLeft: "1rem",
-    paddingRight: "1rem",
-    fontSize: ".875rem",
-    lineHeight: ["1.25rem", "1em"],
-    minHeight: "3rem",
-    gap: ".5rem",
-    fontWeight: 600,
-    textDecorationLine: "none",
-    borderWidth: "thin",
-
-    outlineColor: "base.content",
-
-    _focusVisible: {
-      outlineStyle: "solid",
-      outlineWidth: "2px",
-      outlineOffset: "2px",
-    },
-
-    _disabled: {
-      opacity: 0.5,
-      pointerEvents: "none",
-    },
-  },
-  variants: {
-    variant: {
-      default: {
-        backgroundColor: "base.200",
-        color: "base.content",
-        _hover: {
-          borderColor: "base.300",
-          backgroundColor: "base.300",
-        },
-      },
-      neutral: {
-        borderColor: "neutral",
-        backgroundColor: "neutral",
-        color: "neutral.content",
-        outlineColor: "neutral",
-        _hover: {
-          borderColor: "neutral.focus",
-          backgroundColor: "neutral.focus",
-        },
-      },
-      primary: {
-        borderColor: "primary",
-        backgroundColor: "primary",
-        color: "primary.content",
-        outlineColor: "primary",
-        _hover: {
-          borderColor: "primary.focus",
-          backgroundColor: "primary.focus",
-        },
-      },
-      secondary: {
-        borderColor: "secondary",
-        backgroundColor: "secondary",
-        color: "secondary.content",
-        outlineColor: "secondary",
-        _hover: {
-          borderColor: "secondary.focus",
-          backgroundColor: "secondary.focus",
-        },
-      },
-      accent: {
-        borderColor: "accent",
-        backgroundColor: "accent",
-        color: "accent.content",
-        outlineColor: "accent",
-        _hover: {
-          borderColor: "accent.focus",
-          backgroundColor: "accent.focus",
-        },
-      },
-    },
-    size: {
-      default: { height: "10", px: "4", py: "2" },
-      sm: { height: "9", borderRadius: "md", px: "3" },
-      lg: { height: "11", borderRadius: "md", px: "8" },
-      icon: { height: "10", width: "10" },
-    },
-  },
-  defaultVariants: {
-    variant: "default",
-    size: "default",
+    display: "flex",
+    flex: "1 1 auto",
+    flexDirection: "column",
+    padding: "8",
+    gap: "1",
   },
 });
 
